4dad1f3de2d92369ba0ed5094af87b9dd8ba9d15
anastassow <127148449+anastassow@users.noreply.github.com>
2025-05-24 15:17:25 +0300

    model for vasi

--- server/movie/models.py
+++ server/movie/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 
-# class Movie(models.Model):
+class Media(models.Model):
+    
+    MEDIA_TYPE_CHOICES = [
+        ('anime', 'Anime'),
+        ('series', 'Series'),
+        ('movie', 'Movie'),
+    ]
 
+    type = models.CharField(max_length=10, choices=MEDIA_TYPE_CHOICES)
+    name = models.CharField(max_length=255)
+    vector = models.TextField()
+    external_id = models.CharField(max_length=255, unique=True)
+    provider = models.CharField(max_length=255)
 
