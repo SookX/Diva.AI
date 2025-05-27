dc28ac20f9edaeddb6d85a3ceefef693e50b27be
anastassow <127148449+anastassow@users.noreply.github.com>
2025-05-28 00:16:39 +0300

    register ready

--- client/lib/authentication/register.dart
+++ client/lib/authentication/register.dart
@@ -1,17 +1,28 @@
 import 'dart:convert';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:http/http.dart' as http;
 
-class RegisterView extends StatelessWidget {
+class RegisterView extends StatefulWidget {
   const RegisterView({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterView> createState() => _RegisterViewState();
+}
+
+class _RegisterViewState extends State<RegisterView> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController usernameController = TextEditingController();
 
   Future<void> _register() async {
     final url = Uri.parse('http://10.0.2.2:8000/api/user/register/');
     final headers = {'Content-Type': 'application/json'};
     final body = jsonEncode({
-      'username': 'testuser',
-      'email': 'test@example.com',
-      'password': 'testpassword123',
+      'username': usernameController.text,
+      'email': emailController.text,
+      'password': passwordController.text,
     });
 
     try {
@@ -30,11 +41,195 @@ class RegisterView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Register')),
+      backgroundColor: const Color(0xFF030016),
       body: Center(
-        child: ElevatedButton(
-          onPressed: _register,
-          child: const Text('Register with Hardcoded Data'),
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.w),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(height: 130.h),
+              Text(
+                "Register",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30.sp,
+                ),
+              ),
+              SizedBox(height: 50.h),
+              _buildTextField(
+                controller: usernameController,
+                label: 'Username',
+              ),
+              SizedBox(height: 20.h),
+              _buildTextField(
+                controller: emailController,
+                label: 'Email',
+                keyboardType: TextInputType.emailAddress,
+              ),
+              SizedBox(height: 20.h),
+              _buildTextField(
+                controller: passwordController,
+                label: 'Password',
+                obscureText: true,
+              ),
+              SizedBox(height: 30.h),
+              SizedBox(
+                width: double.infinity,
+                height: 60.h,
+                child: ElevatedButton(
+                  onPressed: _register,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF33296C),
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    padding: EdgeInsets.zero,
+                  ),
+                  child: const Text(
+                    "Register",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20.h),
+              Row(
+                children: [
+                  Expanded(
+                    child: Divider(
+                      color: Color(0xFF121023),
+                      thickness: 1,
+                      endIndent: 20,
+                    ),
+                  ),
+                  const Text(
+                    'or',
+                    style: TextStyle(color: Colors.white70, fontSize: 16),
+                  ),
+                  Expanded(
+                    child: Divider(
+                      color: Color(0xFF121023),
+                      thickness: 1,
+                      indent: 20,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20.h),
+              _socialButton(
+                label: "Continue with Google",
+                logoUrl:
+                    "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/2048px-Google_%22G%22_logo.svg.png",
+                onPressed: () {},
+              ),
+              SizedBox(height: 16.h),
+              _socialButton(
+                label: "Continue with Facebook",
+                logoUrl:
+                    "https://upload.wikimedia.org/wikipedia/commons/0/05/Facebook_Logo_(2019).png",
+                onPressed: () {},
+              ),
+              SizedBox(height: 40.h),
+              RichText(
+                text: TextSpan(
+                  text: "Already have an account? ",
+                  style: TextStyle(color: Colors.white70, fontSize: 16),
+                  children: [
+                    TextSpan(
+                      text: 'Login',
+                      style: TextStyle(
+                        color: Color(0xFF33296C),
+                        fontWeight: FontWeight.bold,
+                      ),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          Navigator.pushNamed(context, '/login');
+                        },
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTextField({
+    required TextEditingController controller,
+    required String label,
+    TextInputType keyboardType = TextInputType.text,
+    bool obscureText = false,
+  }) {
+    return TextField(
+      controller: controller,
+      keyboardType: keyboardType,
+      obscureText: obscureText,
+      style: const TextStyle(color: Colors.white),
+      decoration: InputDecoration(
+        labelText: label,
+        labelStyle: const TextStyle(color: Colors.white70),
+        filled: true,
+        fillColor: Color.fromARGB(255, 19, 17, 40),
+        border: OutlineInputBorder(
+          borderSide: BorderSide.none,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        contentPadding:
+            const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+      ),
+    );
+  }
+
+  Widget _socialButton({
+    required String label,
+    required String logoUrl,
+    required VoidCallback onPressed,
+  }) {
+    return SizedBox(
+      width: double.infinity,
+      height: 60.h,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: const Color.fromARGB(255, 19, 17, 40),
+          elevation: 0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          padding: EdgeInsets.symmetric(horizontal: 16.w),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 1,
+              child: Image.network(
+                logoUrl,
+                height: 23.h,
+                width: 23.h,
+              ),
+            ),
+            SizedBox(width: 12.w),
+            Expanded(
+              flex: 4,
+              child: Text(
+                label,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
